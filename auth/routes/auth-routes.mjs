543bfa71de116e5cca72698847e4c8bedb6f3b22
VonAdamo <148451831+VonAdamo@users.noreply.github.com>
2024-07-07 14:58:05 +0200

    reset password is working

--- auth/routes/auth-routes.mjs
+++ auth/routes/auth-routes.mjs
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/forgotpassword", forgotPassword);
-router.put("/resetpassword/:resettoken", resetPassword);
+router.put("/resetpassword/:token", resetPassword);
 router.get("/me", protect, getMe);
 router.put("/updateuser", protect, updateUserDetails);
 router.put("/updatepassword", protect, updatePassword);
